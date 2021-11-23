926b373c5dafc7790abb84e29318010e1bfb25ab
PiedPiperCaos <cmarmeg382@g.educaand.es>
2021-11-23 14:44:37 +0100

    Final clase martes 23-11

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -6,25 +6,88 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            child: Column(
+      body: Container(
+        child: Stack(
+          children: [
+            Column(
               children: [
-                Expanded(
-                    flex: 2,
-                    child: Container(
-                      color: Colors.blue,
-                    )),
-                Expanded(
-                    flex: 8,
-                    child: Container(
-                      color: Colors.white,
-                    ))
+                Background(),
               ],
             ),
-          ),
-        ],
+            Container(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Text(
+                      'iSéneca',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 50,
+                      ),
+                    ),
+                    Container(
+                      color: Colors.red,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(child: Text('Martinez Megias, Carlos')),
+                              Expanded(
+                                child: Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Icon(Icons.people)),
+                              )
+                            ],
+                          ),
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Column(
+                              children: [
+                                Text('I.E.S. Jándula'),
+                                Text('Perfil dirección')
+                              ],
+                            ),
+                          ),
+                          Row()
+                        ],
+                      ),
+                    ),
+                    Container()
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Expanded(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Container(
+                  color: Color.fromARGB(255, 1, 84, 156),
+                )),
+            Expanded(
+                flex: 8,
+                child: Container(
+                  color: Colors.white,
+                ))
+          ],
+        ),
       ),
     );
   }
